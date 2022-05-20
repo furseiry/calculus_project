5e7406c78e2600e03e41f40f0557ecd83db40392
afredge <afrederick23@rthighschool.org>
2022-05-20 12:44:56 -0400

    Add Polynomial functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use calculus_project::numerical_function::*;
+
 fn main() {
-    println!("Hello, world!");
+    let f = Polynomial::new(vec![1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]);
+    println!("{:#?}", f);
+    println!("{}", f.value(6.));
+    println!("{}", f.derive(6.));
+    println!("{}", f.nth_derivative(10).value(1.))
 }
